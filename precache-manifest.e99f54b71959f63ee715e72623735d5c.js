aa0b0447ab3b803c62565fa00ac1b5975c0871ef
EmmanuelSal <es1516@ic.ac.uk>
2021-06-02 19:44:12 +0100

    Updates

---
+++ precache-manifest.e99f54b71959f63ee715e72623735d5c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "7daf14f7d888d17731e9",
+    "url": "/FinalLearningDashboard/static/css/main.0373a479.chunk.css"
+  },
+  {
+    "revision": "7daf14f7d888d17731e9",
+    "url": "/FinalLearningDashboard/static/js/main.7daf14f7.chunk.js"
+  },
+  {
+    "revision": "0dfb8f1e5e566bd460d7",
+    "url": "/FinalLearningDashboard/static/css/1.4d5ea8a0.chunk.css"
+  },
+  {
+    "revision": "0dfb8f1e5e566bd460d7",
+    "url": "/FinalLearningDashboard/static/js/1.0dfb8f1e.chunk.js"
+  },
+  {
+    "revision": "8fb4176d9f7522ff4ed5",
+    "url": "/FinalLearningDashboard/static/js/runtime~main.8fb4176d.js"
+  },
+  {
+    "revision": "64db2776a5a125ba817dd03f5936f241",
+    "url": "/FinalLearningDashboard/static/media/0.64db2776.jpg"
+  },
+  {
+    "revision": "60a859917fe65ea1f9f070e0dc080c76",
+    "url": "/FinalLearningDashboard/static/media/shards-dashboards-logo.60a85991.svg"
+  },
+  {
+    "revision": "56606f8d4d205b498fe56fe1599d6602",
+    "url": "/FinalLearningDashboard/static/media/1.56606f8d.jpg"
+  },
+  {
+    "revision": "e26f6eb5f904f9dcfadd704cc006ead3",
+    "url": "/FinalLearningDashboard/static/media/2.e26f6eb5.jpg"
+  },
+  {
+    "revision": "1b203a8339857894a72e2e87e93cfb60",
+    "url": "/FinalLearningDashboard/static/media/3.1b203a83.jpg"
+  },
+  {
+    "revision": "330ff49862b84b19b38c29663897b532",
+    "url": "/FinalLearningDashboard/index.html"
+  }
+];
